8a726ab2343be206faeee07f496781c240e41b1a
yorickburgers <yorickburgers@live.nl>
2025-06-22 11:41:44 +0200

    startup project

---
+++ src/main/java/nl/novi/baccampsite/BacCampsiteApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.baccampsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacCampsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacCampsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/baccampsite/BacCampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.baccampsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacCampsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

7d77c354b5366252a147b1de5449266470a367b7
yorickburgers <yorickburgers@live.nl>
2025-07-02 13:52:30 +0200

    removed duplicate code

--- src/main/java/nl/novi/baccampsite/controllers/CharacterController.java
+++ src/main/java/nl/novi/baccampsite/controllers/CharacterController.java
@@ -9,46 +9,45 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@RestController
+@RequestMapping("/characters")
 public class CharacterController {
-    @RestController
-    @RequestMapping("/characters")
-    public class CharacterController {
 
-        private final CharacterService characterService;
+    private final CharacterService characterService;
 
-        public CharacterController(CharacterService characterService) {
-            this.characterService = characterService;
-        }
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
 
-        @GetMapping
-        public ResponseEntity<List<CharacterResponseDto>> retrieveCharacters(@RequestParam Long userId) {
-            return ResponseEntity.ok(characterService.retrieveCharacters(userId));
-        }
+    @GetMapping
+    public ResponseEntity<List<CharacterResponseDto>> retrieveCharacters(@RequestParam Long userId) {
+        return ResponseEntity.ok(characterService.retrieveCharacters(userId));
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<CharacterResponseDto>  retrieveCharacter(@PathVariable Long id) {
-            return ResponseEntity.ok(characterService.retrieveCharacter(id));
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<CharacterResponseDto>  retrieveCharacter(@PathVariable Long id) {
+        return ResponseEntity.ok(characterService.retrieveCharacter(id));
+    }
 
-        @PostMapping
-        public ResponseEntity<CharacterResponseDto> createCharacter(@RequestBody CharacterRequestDto characterRequestDto) {
-            CharacterResponseDto characterResponseDto = characterService.createCharacter(characterRequestDto);
+    @PostMapping
+    public ResponseEntity<CharacterResponseDto> createCharacter(@RequestBody CharacterRequestDto characterRequestDto) {
+        CharacterResponseDto characterResponseDto = characterService.createCharacter(characterRequestDto);
 
-            URI uri = URI.create(
-                    ServletUriComponentsBuilder
-                            .fromCurrentRequest()
-                            .path("/" + characterResponseDto.id).toUriString());
+        URI uri = URI.create(
+                ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/" + characterResponseDto.id).toUriString());
 
-            return ResponseEntity.created(uri).body(characterResponseDto);
-        }
+        return ResponseEntity.created(uri).body(characterResponseDto);
+    }
 
-        @PutMapping("/{id}")
-        public ResponseEntity<CharacterResponseDto>  updateCharacter(@PathVariable Long id, @RequestBody CharacterRequestDto characterRequestDto) {
-            return ResponseEntity.ok().body(characterService.updateCharacter(id, characterRequestDto));
-        }
+    @PutMapping("/{id}")
+    public ResponseEntity<CharacterResponseDto>  updateCharacter(@PathVariable Long id, @RequestBody CharacterRequestDto characterRequestDto) {
+        return ResponseEntity.ok().body(characterService.updateCharacter(id, characterRequestDto));
+    }
 
-        @DeleteMapping("/{id}")
-        public ResponseEntity<String> deleteCharacter(@PathVariable Long id) {
-            return ResponseEntity.ok(characterService.deleteCharacter(id));
-        }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCharacter(@PathVariable Long id) {
+        return ResponseEntity.ok(characterService.deleteCharacter(id));
+    }
 }

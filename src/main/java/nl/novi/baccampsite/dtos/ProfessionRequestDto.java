fe971c857d8d113750ecb1eaadc39e6e4d1b842e
yorickburgers <yorickburgers@live.nl>
2025-07-02 11:56:43 +0200

    Profession dtos and mapper

--- src/main/java/nl/novi/baccampsite/dtos/ProfessionRequestDto.java
+++ src/main/java/nl/novi/baccampsite/dtos/ProfessionRequestDto.java
@@ -1,4 +1,12 @@
 package nl.novi.baccampsite.dtos;
 
 public class ProfessionRequestDto {
+    public String name;
+    public String description;
+    public int hpModifier;
+    public String primaryAbility;
+    public String traitOne;
+    public String traitTwo;
+    public String traitThree;
+    public String traitFour;
 }

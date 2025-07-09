6646582eb1ec52432890c8cbf9d88e9fd35771cf
yorickburgers <yorickburgers@live.nl>
2025-07-09 18:16:17 +0200

    added characters to campaign DTO and modified mapper

--- src/main/java/nl/novi/baccampsite/mappers/CampaignMapper.java
+++ src/main/java/nl/novi/baccampsite/mappers/CampaignMapper.java
@@ -4,12 +4,17 @@ import nl.novi.baccampsite.dtos.CampaignRequestDto;
 import nl.novi.baccampsite.dtos.CampaignResponseDto;
 import nl.novi.baccampsite.models.Campaign;
 
+import java.util.ArrayList;
+
 public class CampaignMapper {
     public static CampaignResponseDto toCampaignResponseDto(Campaign campaign) {
         CampaignResponseDto dto = new CampaignResponseDto();
         dto.id = campaign.getId();
         dto.name = campaign.getName();
         dto.description = campaign.getDescription();
+        dto.characters = new ArrayList<>();
+                campaign.getCharacters().forEach((character) ->
+                        dto.characters.add(CharacterMapper.toCharacterSummaryDto(character)));
         return dto;
     }
 

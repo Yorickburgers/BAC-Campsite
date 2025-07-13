6cd02ca5b2b2143793d77e25536a877b13df6dcc
yorickburgers <yorickburgers@live.nl>
2025-07-13 18:10:47 +0200

    added BadRequest and UsernameNotFoundExceptions

---
+++ src/main/java/nl/novi/baccampsite/exceptions/UsernameNotFoundException.java
@@ -0,0 +1,8 @@
+package nl.novi.baccampsite.exceptions;
+
+public class UsernameNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public UsernameNotFoundException(String username) {
+        super("Cannot find user " + username);
+    }
+}
